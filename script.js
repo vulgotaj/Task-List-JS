a3d1940a4fdda91fb9e64b49cf1344bf2df11ee2
Felipe Tajima <felipebtajima05@gmail.com>
2025-05-22 20:18:36 -0300

    Creating modal

--- script.js
+++ script.js
@@ -2,23 +2,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const menu = document.querySelector('#menu');
     const menuButton = document.querySelector('#menu_button');
     const closeButton = document.querySelector('#close_menu');
-    const menuOverlay = document.querySelector('#menu_overlay');
 
     menuButton.addEventListener('click', () => {
         menu.classList.remove('-translate-x-full');
         menu.classList.add("active");
-        menuOverlay.style.display = "block";
     });
 
     closeButton.addEventListener('click', () => {
         menu.classList.remove("active");
         menu.classList.add('-translate-x-full');
-        menuOverlay.style.display = "none";
     });
 
     menuOverlay.addEventListener('click', () => {
         menu.classList.remove("active");
         menu.classList.add('-translate-x-full');
-        menuOverlay.style.display = "none";
     });
 });
