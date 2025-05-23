1be39d29ec4dea8c65e885cf84ad0eda60867be5
Felipe Tajima <felipebtajima05@gmail.com>
2025-05-23 17:55:55 -0300

    Modal Working + Clean Code

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
+const modal = document.querySelector('#create-modal');
+const modalOpener = document.querySelector('#modal-button');
+const menu = document.querySelector('#menu');
+const menuButton = document.querySelector('#menu_button');
+const closeButton = document.querySelector('#close_menu');
+const menuOverlay = document.querySelector('#menu_overlay');
+
+// ------------------------------------------------------------------------
+
 document.addEventListener("DOMContentLoaded", function () {
-    const menu = document.querySelector('#menu');
-    const menuButton = document.querySelector('#menu_button');
-    const closeButton = document.querySelector('#close_menu');
-
-    menuButton.addEventListener('click', () => {
-        menu.classList.remove('-translate-x-full');
-        menu.classList.add("active");
-    });
-
-    closeButton.addEventListener('click', () => {
-        menu.classList.remove("active");
-        menu.classList.add('-translate-x-full');
-    });
-
-    menuOverlay.addEventListener('click', () => {
-        menu.classList.remove("active");
-        menu.classList.add('-translate-x-full');
-    });
+    menuButton.addEventListener('click', openMenu);
+    closeButton.addEventListener('click', closeMenu);
+    menuOverlay.addEventListener('click', closeMenu);
 });
+
+modalOpener.addEventListener('click', openModal)
+modal.addEventListener('click', closeModal)
+    
+// ------------------------------------------------------------------------
+
+function openModal() {
+    modal.classList.remove('hidden');             
+    modal.classList.add('flex'); 
+}
+
+function closeModal(e) {
+    const cancelButton = document.querySelector('#cancel-button')
+    if(e.target === modal || e.target === cancelButton) {
+        modal.classList.remove('flex');
+        modal.classList.add('hidden');
+    }
+}
+
+function openMenu() {
+    menu.classList.remove('-translate-x-full');
+    menu.classList.add("active");
+    menuOverlay.style.display = 'block'
+}
+
+function closeMenu() {
+    menu.classList.remove("active");
+    menu.classList.add('-translate-x-full');
+    menuOverlay.style.display = 'none';
+}
